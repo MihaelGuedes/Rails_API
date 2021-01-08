74051eeeca83a459eb09ba8c1ee56db653535758
Mihael Guedes <megb@cin.ufpe.br>
2021-01-08 12:19:23 -0300

    Update Neasted Attributes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,9 @@
 class Contact < ApplicationRecord
+    #Associations
     belongs_to :kind #optional: true
     has_many :phones
 
+    accepts_nested_attributes_for :phones, allow_destroy: true
 
     def as_json(options={})
         h = super(options)
